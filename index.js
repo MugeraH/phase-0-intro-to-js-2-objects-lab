ea2a5f62409fca4ef5426dc6402364f882407fe6
Hughes Mugera <hughes.mugera@touchinspiration.com>
2022-03-30 13:48:24 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+let employee = {};
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+  const newObject = { ...obj };
+  newObject[key] = value;
+
+  return newObject;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+  obj[key] = value;
+
+  return obj;
+}
+
+function deleteFromEmployeeByKey(obj, key) {
+  const newObject = { ...obj };
+
+  delete newObject[key];
+
+  return newObject;
+}
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+  delete obj[key];
+
+  return obj;
+}
